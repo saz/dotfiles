16b37ee0d30c104ea5e4c8f149c8b1b1e0f2831a
Steffen Zieger <me@saz.sh>
2012-03-07 15:54:11 +0100

    run awesome config only, if awesome is installed

--- install.sh
+++ install.sh
@@ -44,10 +44,12 @@ git submodule foreach git submodule init
 git submodule foreach git submodule update
 
 # Awesome and Obvious specific stuff
-awesome_version=`awesome -v|grep awesome|cut -d' ' -f2`
-cd ${HOME}/.config/awesome/obvious
-git checkout $awesome_version > /dev/null 2>&1
-if [ $? -eq 1 ]; then
-    git checkout v3.4.10
+if [ -e "/usr/bin/awesome" ]; then
+    awesome_version=`awesome -v|grep awesome|cut -d' ' -f2`
+    cd ${HOME}/.config/awesome/obvious
+    git checkout $awesome_version > /dev/null 2>&1
+    if [ $? -eq 1 ]; then
+        git checkout v3.4.10
+    fi
 fi
 cd -
